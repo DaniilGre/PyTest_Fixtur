a750c66ee83542b628b5d5c6c046331fe03dd5e3
DanielLite <hryshkod@gmail.com>
2021-04-20 18:16:49 +0300

    add normal name and chromedriver

--- test_items.py
+++ test_items.py
@@ -3,7 +3,7 @@
 from time import sleep
 
 
-def test_item(browser):
+def test_add_product_to_basket(browser):
     link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
     browser.get(link)
     sleep(30)
